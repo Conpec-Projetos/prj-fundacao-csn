d51cff0f9c719fc7ebdc6dba100674fb0b9823f2
Karen Ketelhute <karen.rosas@conpec.com.br>
2025-04-28 09:07:35 -0300

    fixed the header in the login and sign in (makgin anter file called headerWrapper

---
+++ src/components/header/headerWrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+// Importing necessary modules and components
+import Header from "@/components/header/header";
+import { usePathname } from "next/navigation";
+
+// This component wraps the Header component and conditionally renders it based on the current route 
+// (é para fazer com que determinadas páginas nn tenham header)
+export default function HeaderWrapper({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+    const noHeaderRoutes = ["/login", "/signin"];
+    const hasHeader = !noHeaderRoutes.includes(pathname); // Check if the header should be rendered
+
+    return (
+    <>
+        {/* Render the Header only if hasHeader is true */}
+        {hasHeader && <Header />}
+        {/* Add padding only if the Header is rendered */}
+        <div className={hasHeader ? "pt-[10vh]" : ""}>
+        {children}
+        </div>
+    </>
+    );
+}
