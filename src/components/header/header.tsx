acfdd78e2eb35135814ff33bb3079d9f2824e13b
mboson <marcos.boson@conpec.com.br>
2025-04-29 20:46:37 -0300

    Feat: implemented the dark mode switch in the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,34 +1,62 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Sun, Moon } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
 
 
 export default function Header(){
     const router = useRouter();
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        let savedMode = localStorage.getItem("displayMode");
+        if(!savedMode) {
+            const newMode = "light"
+            setDarkMode(false)
+            localStorage.setItem("displayMode", newMode)
+        }
+        setDarkMode(savedMode === 'dark' ? true : false);   
+    }, [])
+
+    const toggleDisplayMode = () => {
+        setDarkMode(!darkMode);
+    }
+
     return(
-    <header className="fixed flex flex-col justify-center w-full h-[10vh] bg-blue-fcsn">
-        
-        <div className="flex flex-row justify-evenly w-[85%] text-xl sm:w-3/4 lg:w-1/2 lg:text-2xl text-white font-bold">
+    <body className={`${darkMode ? "dark" : ""}`}>
+        <header className="fixed flex flex-row justify-evenly w-full h-[10vh] bg-blue-fcsn dark:bg-red-500">
             
-            <button
-                onClick={(event) => {
-                    event.preventDefault();
-                    router.push("/")
-                }}
+            <div className="flex flex-row justify-start items-center w-[85%] text-xl gap-4 sm:w-3/4 lg:w-1/2 lg:text-2xl text-white font-bold ml-1">
                 
-                className="
-                    cursor-pointer">Início</button> 
+                <button
+                    onClick={(event) => {
+                        event.preventDefault();
+                        router.push("/")
+                    }}
+                    
+                    className="
+                        cursor-pointer">Início</button> 
+                    
+                <div className="hidden sm:block h-[4vh] w-px bg-gray-300 my-5"></div>
+
+                <button
+                    className="
+                        cursor-pointer">Dashboard</button>
                 
-            <div className="h-[4vh] w-px bg-gray-300"></div>
+                <div className="hidden sm:block h-[4vh] w-px bg-gray-300 my-5"></div>
 
-            <button
-                className="
-                    cursor-pointer">Dashboard</button>
-            
-            <div className="h-[4vh] w-px bg-gray-300"></div>
+                <button className="
+                    cursor-pointer">Projetos</button>
+            </div>
+            <div className="w-[15%] flex justify-end items-center">
+                <button className="cursor-pointer" 
+                onClick={()=>{toggleDisplayMode()}}>{darkMode ? <Sun size={20} /> : <Moon size={20} />}
+                </button>
+            </div>
 
-            <button className="
-                cursor-pointer">Projetos</button>
-        </div>
-    </header>
+        </header>
+    </body>
     );
 }
