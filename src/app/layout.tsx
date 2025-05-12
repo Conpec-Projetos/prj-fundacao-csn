d5761b8d17a71072a460aa3b29e314ed2b2f9665
mboson <marcos.boson@conpec.com.br>
2025-05-12 11:27:08 -0300

    Feat: started implementing the promote to admin function in the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,7 @@ export default function RootLayout({
           <Header />
           {/* Wrap children with HeaderWrapper */}
             <HeaderWrapper>
-              <div className="pt-[10vh]">
-                {children}
-              </div>
+              {children}
             </HeaderWrapper>
         </ThemeProvider>
       </body>
