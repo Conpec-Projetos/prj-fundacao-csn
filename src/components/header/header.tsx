aca0fdac5e39c377f315629e4383a04372e3e9e8
mboson <marcos.boson@conpec.com.br>
2025-05-12 16:18:57 -0300

    Feat: continued implementing the promote user to admin pop up

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,13 +11,13 @@ interface promoteAdminProps {
 }
 
 const PromoteAdmin = ({ id, name }: promoteAdminProps) => (
-    <div className="flex flex-col items-center justify-center w-fit h-fit bg-blue-fcsn3 rounded shadow-md text-white-off text-lg font-bold m-2 text-center">
-        Você deseja promover o colaborador {name} a administrador?
+    <div className="flex flex-col items-center justify-center w-[98%] h-fit bg-blue-fcsn3 rounded shadow-md text-white-off text-lg font-bold m-2 text-center">
+        <span className="m-2">Você deseja promover o colaborador {name} a administrador?</span>
         <div className="flex flex-row gap-2">
-            <button className="bg-green-500 text-white rounded-full px-4 py-2 mt-2 mb-2">
+            <button className="bg-green-500 text-white rounded-full px-4 py-2 mt-2 mb-2 cursor-pointer">
                 Promover
             </button>
-            <button className="bg-red-500 text-white rounded-full px-4 py-2 mt-2 mb-2">
+            <button className="bg-red-500 text-white rounded-full px-4 py-2 mt-2 mb-2 cursor-pointer">
                 Não promover
             </button>
         </div>
@@ -31,6 +31,16 @@ export default function Header(){
     const { darkMode, toggleDarkMode } = useTheme();
     const [isOpen, setIsOpen] = useState(false);
     const [isPopUpOpen, setIsPopUpOpen] = useState(false);
+
+    const UsersToPromote: promoteAdminProps[] = [
+        { id: 1, name: "Teste da Silva" },
+        { id: 2, name: "Testão da Silva" },
+        { id: 3, name: "Testinho da Silva" },
+        { id: 4, name: "Testezinho da Silva" },
+        { id: 5, name: "Testezão da Silva" },
+        { id: 6, name: "Testezãozão da Silva" },
+        { id: 7, name: "Testezinhozinho da Silva" },
+    ]
 
     return(
         <div className={`${darkMode ? "dark" : ""}`} suppressHydrationWarning={true}>
@@ -57,41 +67,59 @@ export default function Header(){
 
                     <button className="
                         cursor-pointer">Projetos</button>
+
+                    <div className="hidden sm:block h-[2vh] w-px bg-white dark:bg-white-off my-5"></div>
+
+                    <button onClick={() => setIsPopUpOpen(!isPopUpOpen)}
+                        className="cursor-pointer">Promover Colaborador</button>
                 </div>
 
                 <button className="sm:hidden" onClick={() => setIsOpen(!isOpen)}>
                     <FaBars size={20} className="text-white" />
                 </button>
 
-                { isOpen && (
-                    <div className="absolute top-[10.5vh] left-0 w-[50%] h-fit bg-blue-fcsn dark:bg-blue-fcsn2 rounded shadow-md text-white-off text-lg font-bold flex flex-col items-center gap-4 p-4">
+                {/* Mobile Menu */}
+                {isOpen && (
+                    <div className="sm:hidden absolute top-[10.5vh] left-1 w-[50%] h-fit bg-blue-fcsn dark:bg-blue-fcsn2 rounded shadow-md text-white-off text-lg font-bold flex flex-col items-center gap-4 p-4">
                         <button
-                        onClick={(event) => {
-                            event.preventDefault();
-                            router.push("/")
-                        }}
-                        
-                        className="cursor-pointer">Início</button> 
-                        
-                        <button onClick={(event) => {
-                            event.preventDefault();
-                            router.push("/dashboard")
-                        }}
-                        className="cursor-pointer">Dashboard</button>
+                            onClick={(event) => {
+                                event.preventDefault();
+                                router.push("/");
+                            }}
+                            className="cursor-pointer"
+                        >
+                            Início
+                        </button>
+
+                        <button
+                            onClick={(event) => {
+                                event.preventDefault();
+                                router.push("/dashboard");
+                            }}
+                            className="cursor-pointer"
+                        >
+                            Dashboard
+                        </button>
 
                         <button className="cursor-pointer">Projetos</button>
 
-                        <button onClick={() => setIsPopUpOpen(!isPopUpOpen)}
-                        className="cursor-pointer">Promover Colaborador</button>
+                        <button
+                            onClick={() => setIsPopUpOpen(!isPopUpOpen)}
+                            className="cursor-pointer"
+                        >
+                            Promover Colaborador
+                        </button>
                     </div>
                 )}
 
-                {isPopUpOpen && isOpen && (
-                            <div className="absolute top-80 left-1 rounded shadow-md bg-blue-fcsn2 max-w-[90%] w-fit h-fit">
-                                {PromoteAdmin({ id: 1, name: "Teste da Silva" })}
-                            </div>
-                        )}
-
+                {/* Promote user */}
+                { isPopUpOpen && (
+                    <div className="flex flex-col justify-center items-center absolute top-78 left-1 sm:top-[10.5vh] sm:left-135 rounded shadow-md bg-blue-fcsn2 max-w-[90%] w-fit h-fit max-h-[90svh] overflow-y-auto overflow-hidden">
+                        {UsersToPromote.map(user => (
+                            <PromoteAdmin key={user.id} id={user.id} name={user.name}/>
+                        ))}
+                    </div>
+                )}
                 
                 <div className="w-[15%] flex justify-end items-center mr-1 sm:mr-3">
                     <button className="cursor-pointer transition-all duration-300" 
