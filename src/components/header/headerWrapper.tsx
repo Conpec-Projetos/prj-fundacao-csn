ecbf97d3ffbebdda4a5f7bf9c58bbce78b819f48
LeonardoF-Conpec <leonardo.franco@conpec.com.br>
2025-07-09 22:48:47 -0300

    feat: redirecionamento ao enviar formulários e header no forms de cadastro caso usuario esteja logado

--- src/components/header/headerWrapper.tsx
+++ src/components/header/headerWrapper.tsx
@@ -1,21 +1,46 @@
 "use client";
-// Importing necessary modules and components
+
 import Header from "@/components/header/header";
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/firebase/firebase-config";
 
 // This component wraps the Header component and conditionally renders it based on the current route 
 // (é para fazer com que determinadas páginas nn tenham header)
 export default function HeaderWrapper({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
-    const noHeaderRoutes = ["/login", "/signin", "/forms-cadastro", "/forms-acompanhamento"];
-    const hasHeader = !noHeaderRoutes.includes(pathname); // Check if the header should be rendered
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [authChecked, setAuthChecked] = useState(false);
+    const noHeaderRoutes = ["/login", "/signin"];
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            // Define o estado como true apenas se houver um usuário e o email for verificado
+            setIsLoggedIn(!!user && !!user.emailVerified);
+            setAuthChecked(true); // Marca que a verificação de auth foi feita
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    let showHeader = false;
+
+    if (authChecked) {
+        if (pathname.startsWith('/forms-cadastro')) {
+            // No forms de cadastro o header só aparece se o usuário estiver logado
+            showHeader = isLoggedIn;
+        } else {
+            showHeader = !noHeaderRoutes.some(route => pathname.startsWith(route));
+        }
+    }
 
     return (
     <>
         {/* Render the Header only if hasHeader is true */}
-        {hasHeader && <Header/>}
+        {showHeader && <Header/>}
         {/* Add padding only if the Header is rendered */}
-        <div className={hasHeader ? "pt-[10vh]" : ""}>
+        <div className={showHeader ? "pt-[10vh]" : ""}>
         {children}
         </div>
     </>
