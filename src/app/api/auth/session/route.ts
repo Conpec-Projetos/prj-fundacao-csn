15c57340bf20368e303c149fb02d4ea5b0dcb5e6
Priscilla Almeida <p176205@dac.unicamp.br>
2025-07-14 16:52:37 -0300

    tentando fazer o middleware

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,20 +1,68 @@
 import { NextResponse } from 'next/server'; //usado para retornar uma resposta HTTP na API do Next.js.
 import { cookies } from 'next/headers';
 import { authAdmin } from '@/firebase/firebase-admin-config';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '@/firebase/firebase-config';
 
 // Essa funcao sera chamada quando o frontend enviar uma requisicao para a rota (api/auth/session)
 export async function POST(request: Request) {
-  // Apos o signin o firebase gerará um idToken
+  // Apos o signin o firebase gerará um idToken, ao fazer o login fazemos uma requisicao para essa api passando esse idTpken que usaremos para criar o cookie
   const { idToken } = await request.json();
 
   if (!idToken) {
     return NextResponse.json({ error: 'ID token not provided.' }, { status: 400 });
   }
 
-  // Define o tempo de expiração da sessão em milissegundos (5 dias)
-  const expiresIn = 60 * 60 * 24 * 5 * 1000;
-
+  // Na rota da API
+console.log("Iniciando POST /api/auth/session");
   try {
+    // Com o firebase-admin conseguimos decodificar o token e pegar informacoes como email e id
+    const decoded = await authAdmin.verifyIdToken(idToken);
+    const uid = decoded.uid;
+    const email = decoded.email;
+
+    if (!email) {
+      return NextResponse.json({ error: "Email não disponível no token." }, { status: 400 });
+    }
+
+    // Aqui vamos fazer uma claim (“atributo” incluído dentro do token), precisamos usar isso para nao precisarmos no middleware fazer uma requisicao ao firestore
+    // Verifica se já tem o claim
+    const alreadyHasClaim = decoded.admin === true;
+
+    // Consulta Firestore apenas se ainda não tem o claim
+    if (!alreadyHasClaim) {
+      const dominiosInternos = ["conpec.com.br", "csn.com.br", "fundacaocsn.org.br"];
+      const domain = email.split("@")[1];
+      const isInterno = dominiosInternos.includes(domain);
+
+      if(isInterno){
+
+        const usuarioInt = collection(db, "usuarioInt");
+        const qADM = query(usuarioInt, where("email", "==", email), where("administrador", "==", true));
+        const snapshotADM = await getDocs(qADM);
+
+        const isAdmin = !snapshotADM.empty;
+
+        if (isAdmin) {
+          await authAdmin.setCustomUserClaims(uid, { userIntAdmin: true, userExt: false }); // Propriedade que esta agora no token para sabermos se o userInt é adm
+
+          // Retorne que o front precisa renovar o token antes de criar o cookie, pois atualizamos ele com a claim
+          return NextResponse.json({ mustRefreshToken: true});
+        }
+        else {
+          await authAdmin.setCustomUserClaims(uid, { userIntAdmin: false, userExt: false });
+          // Retorne que o front precisa renovar o token antes de criar o cookie, pois atualizamos ele com a claim
+          return NextResponse.json({ mustRefreshToken: true });
+        }
+      } else {
+          await authAdmin.setCustomUserClaims(uid, { userIntAdmin: false, userExt: true }); // Propriedade que esta agora no token para sabermos se o userInt é adm
+
+          // Retorne que o front precisa renovar o token antes de criar o cookie, pois atualizamos ele com a claim
+          return NextResponse.json({ mustRefreshToken: true});
+      }
+    }
+    // Define o tempo de expiração da sessão em milissegundos (5 dias)
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
     // O idToken (do Firebase Auth) é trocado por um session cookie, que pode ser verificado no servidor depois (authAdmin.createSessionCookie é uma função do Firebase Admin)
     const sessionCookie = await authAdmin.createSessionCookie(idToken, { expiresIn });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,8 @@
 import AdminHomeClient from "@/components/homeAdmin/homeClient";
-import { getCurrentUser } from "@/lib/auth";
 import { IsADM } from "@/lib/isAdm";
 import { redirect } from "next/navigation";
 
 export default async function AdminHomePage() {
-    const user = await getCurrentUser();
-
-    // Verificação se o usuario esta logado, se nao estiver redirecionamos para a pagina de login
-    if (!user || !user.email_verified || !user.email) {
-        return redirect('/login');
-    }
-
-    const email = user.email;
-    const domain = email.split("@")[1];
-    const adm = await IsADM(email);
-    const internalDomains = ["conpec.com.br", "csn.com.br", "fundacaocsn.org.br"];
-    const isInternalUser = internalDomains.includes(domain);
-
-    // Fluxo de redirecionamento
-    if (!isInternalUser) {
-        return redirect('/inicio-externo'); // Usuários externos
-    }
-    if (!adm) {
-        return redirect('/dashboard'); // Usuários internos não-admin
-    }
-
     // Somente usuarios internos e admins  chegam aqui
     return <AdminHomeClient />;
 }
