6292117e0d5ead99e4e34fa333b70f54212f19d0
Priscilla Almeida <p176205@dac.unicamp.br>
2025-07-29 12:40:32 -0300

    Alteracoes no header para usar a api e pequenas alteracoes no front do header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -31,15 +31,6 @@ const PromoteAdmin = ({name }: promoteAdminProps) => ( //tirei o "id" para conse
     </div>
 );
 
-// Usamos essa funcao IsADM pois nossa funcao que esta dentro da pasta lib utiliza o firebase-admin
-async function IsADM(email: string | null): Promise<boolean> {
-    if (!email) return false;
-    const usuarioInt = collection(db, "usuarioInt");
-    const qADM = query(usuarioInt, where("email", "==", email), where("administrador", "==", true));
-    const snapshotADM = await getDocs(qADM);
-    return !snapshotADM.empty;
-}
-
 export default function Header() {
     const router = useRouter();
     const { darkMode, toggleDarkMode } = useTheme();
@@ -62,9 +53,11 @@ export default function Header() {
     async function fetchUser() {
         const res = await fetch('/api/auth/session', { method: 'GET' });
         const data = await res.json();
-        if (data.user?.email) {
-            const isAdmin = await IsADM(data.user.email);
-            setAdm(isAdmin);
+        console.log("metodo get");
+        console.log(data);
+        console.log(data.user?.userIntAdmin);
+        if (data.user?.userIntAdmin){
+            setAdm(true);
         } else {
             setAdm(false);
         }
@@ -79,8 +72,8 @@ export default function Header() {
     if (adm) {
         return (
             <div className={`${darkMode ? "dark" : ""}`} suppressHydrationWarning={true}>
-                <header className="fixed top-0 flex flex-row justify-between w-full h-[10vh] bg-blue-fcsn2 z-50 shadow-md/20 px-10 mb-56 shadow-lg">
-                    <div className="hidden sm:flex flex-row justify-start items-center w-[85%] text-white dark:text-white-off text-xl gap-4 sm:w-3/4 lg:w-1/2 lg:text-2xl font-bold ml-1">
+                <header className="fixed top-0 flex flex-row justify-between w-full h-[10vh] bg-blue-fcsn2 z-50 shadow-md/20 px-10 mb-56 shadow-lg " >
+                    <div className="hidden md:flex flex-row justify-start items-center w-[85%] text-white dark:text-white-off text-xl gap-4 sm:w-3/4 lg:w-1/2 lg:text-2xl font-bold ml-1">
                         <button onClick={() => router.push("/")} className="cursor-pointer">Início</button>
                         <div className="hidden sm:block h-[2vh] w-px bg-white dark:bg-white-off my-5"></div>
                         <button onClick={() => router.push("/dashboard")} className="cursor-pointer">Dashboard</button>
@@ -90,18 +83,14 @@ export default function Header() {
                         <button onClick={() => setIsPopUpOpen(!isPopUpOpen)} className="cursor-pointer whitespace-nowrap">Promover Colaborador</button>
                     </div>
 
-                    <div className="flex flex-row gap-10">
-                        <button className="sm:hidden" onClick={() => setIsOpen(!isOpen)}>
+                  <div className="flex flex-row items-center w-full justify-between md:justify-end">
+                        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
                             <FaBars size={20} className="text-white" />
                         </button>
 
-                        <div className="py-6">
-                            <Botao_Logout />
-                        </div>
-
                         {/* Mobile Menu */}
                         {isOpen && (
-                            <div className="sm:hidden absolute top-[10.5vh] left-1 w-[50%] h-fit bg-blue-fcsn dark:bg-blue-fcsn2 rounded shadow-md text-white-off text-lg font-bold flex flex-col items-center gap-4 p-4">
+                            <div className="md:hidden absolute top-[10.5vh] left-1 w-[50%] h-fit bg-blue-fcsn dark:bg-blue-fcsn2 rounded shadow-md text-white-off text-lg font-bold flex flex-col items-center gap-4 p-4">
                                 <button onClick={() => router.push("/")} className="cursor-pointer">Início</button>
                                 <button onClick={() => router.push("/dashboard")} className="cursor-pointer">Dashboard</button>
                                 <button onClick={() => router.push("/todos-projetos")} className="cursor-pointer">Projetos</button>
@@ -118,10 +107,16 @@ export default function Header() {
                             </div>
                         )}
 
+                        <div className="flex flex-row gap-6 ">
                         <div className="w-[15%] flex justify-end items-center mr-1 sm:mr-3">
                             <button className="cursor-pointer transition-all duration-300" onClick={toggleDarkMode}>
                                 {darkMode ? <Moon size={20} className="text-white" /> : <Sun size={20} className="text-white" />}
                             </button>
+                        </div>
+
+                        <div className="pt-2 md:pt-1">
+                            <Botao_Logout />
+                        </div>
                         </div>
                     </div>
                 </header>
