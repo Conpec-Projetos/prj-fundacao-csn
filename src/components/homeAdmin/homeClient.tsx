de5d9f08421e2eddf731f1e52218513635dd7cd2
LeonardoF-Conpec <leonardo.franco@conpec.com.br>
2025-08-15 14:28:23 -0300

    fix: homepage dinamica

--- src/components/homeAdmin/homeClient.tsx
+++ src/components/homeAdmin/homeClient.tsx
@@ -32,12 +32,12 @@ const MetricCard: React.FC<MetricCardProps> = ({
   icon,
   color,
 }) => (
-  <div className="bg-white-off dark:bg-blue-fcsn2 rounded-lg shadow-md p-6 flex flex-col items-center justify-center h-full w-full">
+  <div className="bg-white dark:bg-blue-fcsn text-gray-800 dark:text-white-off rounded-lg shadow-md p-6 flex flex-col justify-between">
     <div className={`${colorMap[color]} text-2xl mb-2`}>{icon}</div>
-    <h3 className="text-lg md:text-xl text-blue-fcsn dark:text-white-off font-medium mb-1 text-center ">
+    <h3 className="text-lg font-semibold text-blue-fcsn dark:text-gray-300">
       {title}
     </h3>
-    <p className="text-md md:text-xl whitespace-nowrap font-bold text-blue-fcsn dark:text-white-off">
+    <p className="text-4xl font-bold text-pink-fcsn mt-2">
       {value}
     </p>
   </div>
@@ -53,7 +53,7 @@ export default function AdminHomeClient({ dados, estadosAtendidos }: { dados: da
 
 
   return (
-    <div className="flex flex-col grow min-h-[90vh]">
+    <div className="flex flex-col grow min-h-[90vh] bg-white-off dark:bg-blue-fcsn2">
       <main className="flex flex-col gap-8 px-8 pb-8 flex-1 sm:mx-8 pt-12">
         {/* Seção de boas-vindas */}
         <Greetings userName={userName} />
@@ -89,7 +89,7 @@ export default function AdminHomeClient({ dados, estadosAtendidos }: { dados: da
           </section>
 
           {/* Planilha */}
-          <section className="w-full">
+          <section className="w-full bg-white dark:bg-blue-fcsn p-2 sm:p-4 rounded-lg shadow-lg">
             <Planilha tipoPlanilha="monitoramento" />
           </section>
         </div>
